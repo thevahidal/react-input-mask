dabfe75964390019705850ffe71f715e2524aec8
Vahid Al <vahidtheal@gmail.com>
2021-07-06 00:43:47 +0430

    maskChar input added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const Input = props => {
       let valueChar = value[index]
       let maskChar = props.mask[index]
       if (!valueChar) break
-      if (valueChar !== maskChar) unmaskedValue += valueChar 
+      if (valueChar !== maskChar) unmaskedValue += valueChar
     }
     return unmaskedValue
   }
@@ -32,7 +32,7 @@ const Input = props => {
   }
 
   return (
-    <input 
+    <input
       {...props}
       // type='number'
       value={maskValue(props.value)}
@@ -44,29 +44,52 @@ const Input = props => {
 const App = () => {
   const [value, setValue] = useState('')
   const [mask, setMask] = useState('(+$$$) $$$-$$$$-$$$$')
+  const [maskChar, setMaskChar] = useState('$')
 
   return (
     <div className="App">
-      <div>
-        <h3 className='label'>Mask Template</h3>
-        <input
-          value={mask}
-          onChange={e => setMask(e.target.value)}
-          style={{marginBottom: 50}}
-        />
+      <div style={{ width: '50%' }}>
+        <div className='config'
+          style={{
+            marginBottom: 60,
+          }}
+        >
+          <div style={{
+            width: '20%', position: 'relative',
+          }}
+          >
+            <h3 className='label'>Mask Character</h3>
+            <input
+              value={maskChar}
+              maxLength={1}
+              onChange={e => setMaskChar(e.target.value)}
+            />
+          </div>
+          <div style={{ width: '70%', position: 'relative' }}>
+            <h3 className='label'>Mask Template</h3>
+            <input
+              value={mask}
+              onChange={e => setMask(e.target.value)}
+            />
+          </div>
+        </div>
         <h3 className='label'>Masked Input</h3>
-        <Input 
+        <Input
           value={value}
           onChange={text => setValue(text)}
           mask={mask}
-          maskChar='$'
+          maskChar={maskChar}
           placeholder={mask}
-          style={{marginBottom: 50}}
+          style={{ marginBottom: 30 }}
         />
         <h3 className='label'>Output</h3>
-        <input 
+        <input
           value={value}
           disabled
+          style={{
+            color: '#222',
+            background: '#aaa'
+          }}
         />
       </div>
     </div>
